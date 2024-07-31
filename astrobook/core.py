5a4c064c3a3da0b0db8e8efae04c21f127853f93
Behrouz <behrouz.safari@gmail.com>
2024-07-31 22:37:37 +0200

    Update core.py

--- astrobook/core.py
+++ astrobook/core.py
@@ -2,15 +2,16 @@ import pandas as pd
 from requests import request
 from io import StringIO
 
-SIMBAD_API = 'https://simbad.u-strasbg.fr/simbad/sim-tap/sync?request=doQuery&lang=adql&query='
-IRSA_API = 'https://irsa.ipac.caltech.edu/TAP/sync?QUERY='
+APIs = {
+    'simbad': 'https://simbad.u-strasbg.fr/simbad/sim-tap/sync?request=doQuery&lang=adql&query=',
+    'vizier': 'http://tapvizier.u-strasbg.fr/TAPVizieR/tap/sync?request=doQuery&lang=adql&query=',
+    'irsa': 'https://irsa.ipac.caltech.edu/TAP/sync?QUERY=',
+    }
 
 
 def sql2df(script, api):
-    if api.lower()=='simbad':
-        api = SIMBAD_API
-    elif api.lower()=='irsa':
-        api = IRSA_API
+    if api.lower() in APIs.keys():
+        api = APIs[api]
     script = ' '.join(script.strip().split('\n'))
     url = api + script.replace(' ', '%20') + '&format=csv'
     res = request('GET', url).content.decode('utf-8')
