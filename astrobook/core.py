afc504a808f1ca33c0fc28e239967e8332840ed8
Behrouz <behrouz.safari@gmail.com>
2024-07-31 22:48:11 +0200

    Update core.py

--- astrobook/core.py
+++ astrobook/core.py
@@ -5,6 +5,7 @@ from io import StringIO
 APIs = {
     'simbad': 'https://simbad.u-strasbg.fr/simbad/sim-tap/sync?request=doQuery&lang=adql&query=',
     'vizier': 'http://tapvizier.u-strasbg.fr/TAPVizieR/tap/sync?request=doQuery&lang=adql&query=',
+    'gaia': 'https://gea.esac.esa.int/tap-server/tap/sync?request=doQuery&lang=adql&query=',
     'irsa': 'https://irsa.ipac.caltech.edu/TAP/sync?QUERY=',
     }
 
