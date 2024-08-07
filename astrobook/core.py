00e6fb1c1e29e86ffb262cea8080dc0c2bd401e6
Behrouz <behrouz.safari@gmail.com>
2024-08-07 20:23:46 +0200

    Update core.py

--- astrobook/core.py
+++ astrobook/core.py
@@ -7,6 +7,11 @@ APIs = {
     'vizier': 'http://tapvizier.u-strasbg.fr/TAPVizieR/tap/sync?request=doQuery&lang=adql&query=',
     'gaia': 'https://gea.esac.esa.int/tap-server/tap/sync?request=doQuery&lang=adql&query=',
     'irsa': 'https://irsa.ipac.caltech.edu/TAP/sync?QUERY=',
+    'sdss':  'http://skyserver.sdss.org/dr16/en/tools/search/x_sql.aspx?cmd=',
+    'sdss13': 'http://skyserver.sdss.org/dr13/en/tools/search/x_sql.aspx?cmd=',
+    'sdss16': 'http://skyserver.sdss.org/dr16/en/tools/search/x_sql.aspx?cmd=',
+    'sdss17': 'https://skyserver.sdss.org/dr17/SkyServerWS/SearchTools/SqlSearch?cmd=',
+    'sdss18': 'https://skyserver.sdss.org/dr18/SkyServerWS/SearchTools/SqlSearch?cmd=',
     }
 
 
@@ -16,4 +21,9 @@ def sql2df(script, api):
     script = ' '.join(script.strip().split('\n'))
     url = api + script.replace(' ', '%20') + '&format=csv'
     res = request('GET', url).content.decode('utf-8')
+    if len(res) > 0:
+        if res.split('\n')[0][0] == '#':
+            res = '\n'.join(res.split('\n')[1:])
+    else:
+        raise Exception('Invalid URL!')
     return pd.read_csv(StringIO(res))
